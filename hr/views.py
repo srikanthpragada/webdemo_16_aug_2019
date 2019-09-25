a49242eed8ec18a7f1d03b34b7db8ab6e83225f0
srikanthpragada <srikanthpragada@gmail.com>
2019-09-25 20:58:24 +0530

    25-SEP

---
+++ hr/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def info(request):
+    return render(request,'info.html')
+
+
+def countries_list(request):
+    resp = requests.get("https://restcountries.eu/rest/v2/all")
+    countries = resp.json()
+    countries = sorted(countries, key = lambda c : c['population'], reverse=True)[:10]
+    return render(request,
+                  "countries_list.html",
+                  {"countries" : countries} )
