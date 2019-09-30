8cf58558aadbdd47b0897b6036a2a2bd83f22d22
srikanthpragada <srikanthpragada@gmail.com>
2019-09-30 20:57:27 +0530

    30-SEP

--- hr/models.py
+++ hr/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=30)
+    job = models.CharField(max_length=10, null=True)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} - {self.fullname} - {self.job} - {self.salary}"
